6e6ff03c84d5a725b727571c18f2031f44145bc6
david <d.ncklsr@gmail.com>
2025-07-23 20:10:50 -0500

    uncommenting

--- train.py
+++ train.py
@@ -130,31 +130,20 @@ def optimize_model():
     if len(memory) < BATCH_SIZE:
         return
     transitions = memory.sample(BATCH_SIZE)
-    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
-    # detailed explanation). This converts batch-array of Transitions
-    # to Transition of batch-arrays.
     batch = Transition(*zip(*transitions))
 
-    # Compute a mask of non-final states and concatenate the batch elements
-    # (a final state would've been the one after which simulation ended)
-    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-                                          batch.next_state)), device=device, dtype=torch.bool)
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)),
+                                  device=device, dtype=torch.bool)
     non_final_next_states = torch.cat([s for s in batch.next_state
                                                 if s is not None])
     state_batch = torch.cat(batch.state)
     action_batch = torch.cat(batch.action)
     reward_batch = torch.cat(batch.reward)
 
-    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-    # columns of actions taken. These are the actions which would've been taken
-    # for each batch state according to policy_net
+    # Compute Q(s_t, a)
     state_action_values = policy_net(state_batch).gather(1, action_batch)
 
-    # Compute V(s_{t+1}) for all next states.
-    # Expected values of actions for non_final_next_states are computed based
-    # on the "older" target_net; selecting their best reward with max(1).values
-    # This is merged based on the mask, such that we'll have either the expected
-    # state value or 0 in case the state was final.
+    # Compute V(s_{t+1})
     next_state_values = torch.zeros(BATCH_SIZE, device=device)
     with torch.no_grad():
         next_state_values[non_final_mask] = target_net(non_final_next_states).max(1).values
@@ -165,17 +154,10 @@ def optimize_model():
     criterion = nn.SmoothL1Loss()
     loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
 
-    # Optimize the model
     optimizer.zero_grad()
     loss.backward()
-    # In-place gradient clipping
     torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
     optimizer.step()
-
-
-
-
-
 
 
 
@@ -208,7 +190,8 @@ for i_episode in range(num_episodes):
         if terminated:
             next_state = None
         else:
-            next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+            # already a batch of size 1, because select_action requires also batch format for call of policy_net even it is batch size 1
+            next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0) 
 
         # Store the transition in memory
         memory.push(state, action, next_state, reward)
@@ -217,7 +200,7 @@ for i_episode in range(num_episodes):
         state = next_state
 
 
-        # 2. UPDATE VALUE FUNCTION - UPDATE NN
+        # 2. UPDATE VALUE FUNCTION -> UPDATE NN
         # Perform one step of the optimization (on the policy network)
         optimize_model()
         # Soft update of the target network's weights
