1115dff8dd9d7398386f32127fdbfe1109e2787e
david <d.ncklsr@gmail.com>
2025-09-30 20:38:24 -0400

    adjusted nn further

--- DQN.py
+++ DQN.py
@@ -32,45 +32,16 @@ class ReplayMemory(object):
         return len(self.memory)
     
 
-# class DQN(nn.Module):
-
-#     def __init__(self, h, w, n_actions):
-#         super(DQN, self).__init__()
-#         self.conv1 = nn.Conv2d(1,8,(3,3),padding='same')
-#         self.conv2 = nn.Conv2d(8,16,(3,3),padding='same')
-#         self.pool1 = nn.MaxPool2d((2,2),stride=(2,2))
-#         self.conv3 = nn.Conv2d(16,16,(3,3),padding='same')
-#         self.conv4 = nn.Conv2d(16,16,(3,3),padding='same')
-#         self.pool2 = nn.MaxPool2d((2,2),stride=(2,2))
-#         self.lin1 = nn.Linear(8*8*16, n_actions)
-#         self.lin2 = nn.Linear(256, n_actions)
-
-#     # Called with either one element to determine next action, or a batch
-#     # during optimization. Returns tensor([[left0exp,right0exp]...]).
-#     def forward(self, x):
-#         x = F.relu(self.conv1(x))
-#         x = F.relu(self.conv2(x))
-#         x = self.pool1(x)
-#         # x = F.relu(self.conv3(x))
-#         # x = F.relu(self.conv4(x))
-#         # x = self.pool2(x)
-#         x = x.view(x.size(0),-1)
-#         x = F.relu(self.lin1(x))
-#         # x = F.relu(self.lin2(x))
-#         return x
-    
-
-
 
 class DQN(nn.Module):
 
     def __init__(self, h, w, n_actions):
         super(DQN, self).__init__()
         self.pool1 = nn.MaxPool2d((2,2),stride=(2,2))
-        self.conv1 = nn.Conv2d(1,4,(3,3),padding='same')
-        self.conv2 = nn.Conv2d(4,8,(3,3),padding='same')
-        self.conv3 = nn.Conv2d(8,8,(3,3),padding='same')
-        self.lin1 = nn.Linear(8*8*8, n_actions)
+        self.conv1 = nn.Conv2d(1,8,(3,3),padding='same')
+        self.conv2 = nn.Conv2d(8,16,(3,3),padding='same')
+        self.conv3 = nn.Conv2d(16,16,(3,3),padding='same')
+        self.lin1 = nn.Linear(8*8*16, n_actions)
 
     # Called with either one element to determine next action, or a batch
     # during optimization. Returns tensor([[left0exp,right0exp]...]).
