877f668544bc280cbed57d0130276b944bf37431
david <d.ncklsr@gmail.com>
2025-11-10 18:41:37 -0400

    back to single thread

--- test_grasp.py
+++ test_grasp.py
@@ -1,9 +1,11 @@
 import pybullet as p
+import numpy as np
+import torch
 
 from Robot import Robot
 from Object import Object
 from Env import Env
-
+import train
 
 def test_grasp():
     robot = Robot(TCP_TARGET=(0,0,0))
@@ -11,3 +13,9 @@ def test_grasp():
     env = Env(robot,object,vis=True)
     env.grasp()
     assert env.reward.successfull_grasp()
+
+def test_update_state():
+    obs = np.array([[2,3],[2,4]])
+    state = torch.tensor([[[1,1],[1,1]],[[2,2],[2,2]],[[3,3],[3,3]],[[4,4],[4,4]]])
+    new_state = train.update_state(state,obs)
+    assert new_state == torch.tensor([[[2,2],[2,2]],[[3,3],[3,3]],[[4,4],[4,4]],[[2,3],[2,4]]])
--- train.py
+++ train.py
@@ -24,7 +24,7 @@ from Env import Env
 
 
 
-BATCH_SIZE = 1024 # BATCH_SIZE is the number of transitions sampled from the replay buffer
+BATCH_SIZE = 128 # BATCH_SIZE is the number of transitions sampled from the replay buffer
 GAMMA = 0.99 # GAMMA is the discount factor as mentioned in the previous section
 EPS_START = 0.9 # EPS_START is the starting value of epsilon
 EPS_END = 0.01 # EPS_END is the final value of epsilon
@@ -33,7 +33,6 @@ TAU = 0.005 # TAU is the update rate of the target network
 LR = 0.0003 # LR is the learning rate of the ``AdamW`` optimizer
 
 NUM_EPISODES = 10000
-NUM_CPU = 8
 C=4
 VIS = False
 
@@ -59,16 +58,15 @@ def plot_durations(episode_durations):
 
 
 
-def select_actions(states): # [V,C,H,W]
+def select_action(state): # [N,C,H,W]
     global steps_done
     eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY)
-    steps_done += states.shape[0]
-
+    steps_done += state.shape[0]
     if random.random() > eps_threshold:
         with torch.no_grad():
-            return policy_net(states).max(1).indices # max(1) -> max in dimension 1
+            return policy_net(state).max(1).indices # max(1) -> max in dimension 1
     else:
-        return torch.tensor([random.randrange(n_actions)for _ in range(NUM_CPU)], device=device, dtype=torch.long) # [V]
+        return torch.tensor([random.randrange(n_actions)], device=device, dtype=torch.long) # [N]
 
 
 def optimize_model():
@@ -107,83 +105,67 @@ def optimize_model():
 
 
 
-def update_states(states, obs, keep=None, reset=None): # tensor[V,C,H,W] array[V,H,W]
+def initiate_state(obs): # [H,W]
     obs = torch.as_tensor(obs, dtype=torch.float32, device=device)
-    if keep is not None:
-        keep = torch.as_tensor(keep, dtype=torch.bool, device=device)
-        if keep.any():
-            states = torch.roll(states, shifts=-1, dims=1)
-            states[keep,-1] = obs[keep]
-    if reset is not None:
-        reset = torch.as_tensor(reset, dtype=torch.bool, device=device)
-        if reset.any():
-            states[reset] = obs[reset][:,None,:,:].expand(-1,C,-1,-1)
-    return states
+    state = obs[None,None,:,:].repeat(1,C,1,1)
+    return state # [1,C,H,W]
+
+def update_state(state, obs): # [1,C,H,W] [H,W]
+    obs = torch.as_tensor(obs, dtype=torch.float32, device=device)
+    state = torch.roll(state, shifts=-1, dims=1)
+    state[:,-1] = obs[None,:,:] # with state[:,-1] dimension of state gets reduced to [1,H,W]
+    return state # [1,C,H,W]
 
 
 
 
 
-if __name__ == "__main__":
-    device = torch.device("cpu")
-    vec_env = SubprocVecEnv([lambda: Env(Robot(),Object(),vis=VIS,gamma=GAMMA) for _ in range(NUM_CPU)])
-    n_actions = vec_env.action_space.n
-    h, w = vec_env.observation_space.shape
-    policy_net = DQN(h, w, n_actions).to(device)
-    target_net = DQN(h, w ,n_actions).to(device)
-    target_net.load_state_dict(policy_net.state_dict())
-    optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
-    memory = ReplayMemory(10000)
-    steps_done = 0
-    episode_durations = []
-    episode_durations_cpus = [0]*NUM_CPU
 
 
-    obs = vec_env.reset() # [V,H,W]
-    states = update_states(torch.zeros((NUM_CPU,C,h,w),dtype=torch.float32,device=device), obs, reset=np.ones(NUM_CPU,dtype=bool))
 
-    while len(episode_durations) < NUM_EPISODES:
-        
+device = torch.device("cpu")
+env = Env(Robot(),Object(),vis=VIS,gamma=GAMMA)
+n_actions = env.action_space.n
+h, w = env.observation_space.shape
+policy_net = DQN(h, w, n_actions).to(device)
+target_net = DQN(h, w ,n_actions).to(device)
+target_net.load_state_dict(policy_net.state_dict())
+optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+memory = ReplayMemory(10000)
+steps_done = 0
+episode_durations = []
+
+
+
+
+for i_episode  in range(NUM_EPISODES):
+    obs, info = env.reset()
+    state = initiate_state(obs)
+
+    for t in count():
         # 1. RUN ENVIRONMENT AND PUT INTO REPLAY MEMORY
         # 1.1 run environment
-        actions = select_actions(states)
-        obs, rewards, dones, infos = vec_env.step(actions.tolist())
+        action = select_action(state)
+        obs, reward, terminated, truncated, info = env.step(action.item())
         # 1.2 process results from run and put into memory
-        truncated = np.array([info.get("TimeLimit.truncated") for info in infos], dtype=bool)
-        terminated = dones & ~truncated
-        obs_truncated = np.array([info.get("terminal_observation", np.zeros((h,w))) for info in infos])
-        truncated_states = update_states(states, obs_truncated,truncated)
-        next_states = update_states(states, obs, ~dones, dones)
-        rewards = torch.from_numpy(rewards).to(device=device)
-        for i in range(NUM_CPU):
-            s_i = states[i:i+1]
-            a_i = actions[i:i+1]
-            if truncated[i]:
-                ns_i = truncated_states[i:i+1] # truncated states are stopped because of time limit, so naturally they would progress. but next_state already has observation after reset. so need truncatd_states
-            elif terminated[i]:
-                ns_i = None
-            else:
-                ns_i = next_states[i:i+1]
-            r_i = rewards[i:i+1]
-            memory.push(s_i, a_i, ns_i, r_i) # [N,C,H,W] [N] [N,C,H,W] [N] state, action and reward need N dimension for   torch.cat(batch.state). before 'for i' loop N=V, now N=1
-        # 1.3 update states
-        states = next_states
-
+        reward = torch.tensor([reward]).to(device=device)
+        done = terminated or truncated
+        next_state = update_state(state, obs) if not terminated else None
+        memory.push(state, action, next_state, reward) # [N,C,H,W] [N] [N,C,H,W] [N] state, action and reward need N dimension for   torch.cat(batch.state). before 'for i' loop N=V, now N=1
+        # 1.3 update state
+        state = next_state
 
 
         # 2. TAKE FROM REPLAY MEMORY AND UPDATE NEURAL NETWORK
         # 2.1 Perform one step of the optimization (on the policy network)
         optimize_model()
-
         # 2.2 Soft update of the target network's weights: θ′ ← τ θ + (1 −τ )θ′
         target_net_state_dict = target_net.state_dict()
         policy_net_state_dict = policy_net.state_dict()
         for key in policy_net_state_dict:
             target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
         target_net.load_state_dict(target_net_state_dict)
-
-        episode_durations_cpus=[t+1 for t in episode_durations_cpus]
-        episode_durations = episode_durations + [t for t, done in zip(episode_durations_cpus, dones) if done]
-        episode_durations_cpus = [0 if done else t for t, done in zip(episode_durations_cpus, dones)]
-        if True in dones:
+        if done:
+            episode_durations.append(t+1)
             plot_durations(episode_durations)
+            break
